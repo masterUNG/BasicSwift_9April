4235faa2f7f1c960bad3f003ab7d01c57c51eafb
masterUNG <phrombutr@gmail.com>
2019-04-09 11:12:39 +0700

    Basic Var and let

---
+++ VariableAnConstant.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+//การประกาศตัวแปรแบบ กำหนดค่าจาก Value
+var myName = 123
+
+//การประกาศตัวแปรโดยการกำหนดด้วยตัวเอง
+var mySurname: String = "ABC"
+mySurname = "AAAAAA"
+
+//การประกาศค่าคงที่
+let country: String = "Thailand"
+
+print("Hello World")
+print("Surname = \(mySurname)")
+
+
+
